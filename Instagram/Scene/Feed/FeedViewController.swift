1964966c7dc0519d8976f71f28966e51cabda887
soduma <soduma@naver.com>
2021-12-27 15:02:21 +0900

    image uploadViewController

--- Instagram/Scene/Feed/FeedViewController.swift
+++ Instagram/Scene/Feed/FeedViewController.swift
@@ -37,6 +37,7 @@ extension FeedViewController {
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.modalPresentationStyle = .fullScreen
         imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
     
@@ -58,5 +59,24 @@ extension FeedViewController: UITableViewDataSource {
         cell.selectionStyle = .none
         cell.setUp()
         return cell
+    }
+}
+
+extension FeedViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate { // 두개는 같이 해줘야함
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 이미지 선택 후의 메소드, 이걸 사용할 경우 수동으로 디스미스 해줘야함
+        var selectImage: UIImage?
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectImage = editedImage
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            selectImage = originalImage
+        }
+        
+        dismiss(animated: true) { [weak self] in
+            let uploadViewController = UploadViewController(uploadImage: selectImage ?? UIImage())
+            let navigationController = UINavigationController(rootViewController: uploadViewController)
+            navigationController.modalPresentationStyle = .fullScreen
+            self?.present(navigationController, animated: true, completion: nil)
+        }
     }
 }
---
+++ Instagram/Scene/Feed/UploadViewController.swift
@@ -0,0 +1,80 @@
+//
+//  UploadViewController.swift
+//  Instagram
+//
+//  Created by 장기화 on 2021/12/27.
+//
+
+import UIKit
+import SnapKit
+
+class UploadViewController: UIViewController {
+    private let uploadImage: UIImage
+    private let imageView = UIImageView()
+    private lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.text = "여기에 작성"
+        textView.textColor = .secondaryLabel
+        textView.font = .systemFont(ofSize: 15)
+        textView.delegate = self
+        return textView
+    }()
+    
+    init(uploadImage: UIImage) {
+        self.uploadImage = uploadImage
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        setUpNavigationItem()
+        setUpLayout()
+        imageView.image = uploadImage
+    }
+}
+
+extension UploadViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard textView.textColor == .secondaryLabel else { return }
+        textView.text = nil
+        textView.textColor = .label
+    }
+}
+
+extension UploadViewController {
+    func setUpNavigationItem() {
+        navigationItem.title = "새 게시물"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(tapLeftButton))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "공유", style: .done, target: self, action: #selector(tapRightButton))
+    }
+    
+    @objc func tapLeftButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func tapRightButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setUpLayout() {
+        [imageView, textView].forEach { view.addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.top.leading.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.width.height.equalTo(100)
+        }
+        
+        textView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+            $0.leading.equalTo(imageView.snp.trailing).offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalTo(imageView.snp.bottom)
+        }
+    }
+}
